e73b3638924287f4e09adddf6a0c2926161ee8ee
TheSilentBang1 <113616660+TheSilentBang1@users.noreply.github.com>
2025-04-21 11:49:02 +0000

    dnf algo for sorting 3 distinct values

--- arrays/sort012.cpp
+++ arrays/sort012.cpp
@@ -30,12 +30,33 @@ void sort(vector <int> &arr, int n){
     }
 }
 
+void sortDNF(vector <int> &arr, int n){
+    int low = 0;
+    int mid = 0;
+    int high = n-1;
+    while(mid <= high){
+        if(arr[mid] == 0){
+            swap(arr[mid], arr[low]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid] == 1){
+            mid++;
+        }
+        else{
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
 
 int main(){
 
     vector <int> arr = {2, 0, 2, 1, 1, 0};
     int n = arr.size();
-    sort(arr, n);
+    // sort(arr, n);
+    sortDNF(arr,n);
 
     for(auto i:arr){
         cout<<i<<" ";
